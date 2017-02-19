0e7a81ebe96343bdfe44076b893940131b9bd573
psia12345 <peiksia@gmail.com>
2017-02-19 11:09:08 -0800

    add new home page

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+// const navicon = document.getElementsByClassName('nav-icon')[0];
+// // navicon.addEventListener('click', myFunction())
+// function myFunction(x) {
+//     x.classList.toggle("change");
+//     console.log('clicked');
+// }
 
+  //
+  // $(function(){
+  //     $(".element").typed({
+  //       strings: ["First sentence.", "Second sentence."],
+  //       typeSpeed: 0
+  //     });
+  // });
