52160d2c75b5926fd229eae749d6294f57642601
psia12345 <peiksia@gmail.com>
2017-02-21 08:27:36 -0800

    fix nav icon

--- index.js
+++ index.js
@@ -6,14 +6,3 @@
           x.className = "topnav";
       }
   }
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("content").style.marginLeft = "250px";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("content").style.marginLeft = "0";
-}
